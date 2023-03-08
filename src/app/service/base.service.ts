e664c47a64870b2206b7f46233f1e753fa02c8eb
Moin <cuebix-developer@outlook.com>
2023-03-09 01:10:30 +0500

    add proxy confid changes and base service

---
+++ src/app/service/base.service.ts
@@ -0,0 +1,59 @@
+import { Inject, Injectable, Injector } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { catchError, map, Observable, of } from 'rxjs';
+
+
+@Injectable()
+export abstract class BaseService<T extends any> {
+
+  private http!: HttpClient;
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'
+    })
+  };
+
+  constructor(@Inject(String) public controller: string, inject: Injector) {
+    this.http = inject.get(HttpClient);
+  }
+
+  url = '/api/'
+
+  async Get(methodName: any, params?: any) {
+    if (params) {
+      let url = `${this.url}${this.controller}/${methodName}/${params}`
+      return await this.http.get(url);
+    }
+    else {
+      let url = `${this.url}${this.controller}/${methodName}`
+      return await this.http.get(url);
+    }
+
+  }
+
+  Post(methodName: any, body: any): Observable<any> {
+    let obj: any = {}
+    let url = `${this.url}${this.controller}/${methodName}`
+    return this.http.post(url, body, this.httpOptions).pipe(map((res: any) => {
+      obj.isSuccessFul = true;
+      obj.Data = res;
+      return obj;
+    }), catchError((error: any) => {
+      obj.isSuccessFul = false;
+      obj.Error = error.error.message;
+      return of(obj)
+    })
+    )
+  }
+
+  setController(controllerName: string) {
+    this.controller = controllerName
+  }
+
+  async Delete(id: any) {
+    return await this.http.delete(this.url + this.controller + "/" + id);
+  }
+}
