cf3a0ba4e096b48640aa6ee03a237c8ab00ed230
qusain <qusainqassmi123@gmail.com>
2023-03-10 22:47:23 +0500

    apply changes add admin page

--- src/app/main-layout/main-layout-routing.module.ts
+++ src/app/main-layout/main-layout-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '' , component : LoginComponent },
   { path: 'login' , component : LoginComponent },
+  { path: 'adminLogin' , component : AdminLoginComponent },
   { path: 'signUp' , component : SignUpComponent },
   { path: 'forgetPassword' , component : ForgetPasswordComponent },
 ];
--- src/app/main-layout/main-layout.module.ts
+++ src/app/main-layout/main-layout.module.ts
@@ -6,12 +6,14 @@ import { LoginComponent } from './login/login.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 import { MatModule } from '../material/mat.module';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
     SignUpComponent,
     LoginComponent,
-    ForgetPasswordComponent
+    ForgetPasswordComponent,
+    AdminLoginComponent
   ],
   imports: [
     MainLayoutRoutingModule,
