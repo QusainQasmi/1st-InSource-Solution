bd4e22ef35bd7a3c89d28ed9a795ad34d36419cc
qusain <qusainqassmi123@gmail.com>
2023-03-23 00:44:33 +0500

    Main Page work

--- src/app/pages/user/main-page/main-page.component.ts
+++ src/app/pages/user/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  model: any = {};
+  emailValidator = new FormControl('', [Validators.required, Validators.email]);
 
+  cardsArry: any[] = [
+    {name : 'Tea Set' , src : 'card1.png', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Mug Set' , src : 'card2.png', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Dinner Set' , src : 'card3.png', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Sets' , src : 'card4.jpg', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Tea Set' , src : 'card1.png', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Mug Set' , src : 'card2.png', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Dinner Set' , src : 'card3.png', desc : '$ • American, Restaurant' , rate : '4.5 (413)'},
+    {name : 'Sets' , src : 'card4.jpg', desc : '$ • American, Restaurant' , rate : '4.5 (413)'}
+  ]
 }
