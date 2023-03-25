106a8bc8239a493608d6e70c67fcb8a25fb0eff1
qusain <qusainqassmi123@gmail.com>
2023-03-26 01:31:36 +0500

    Add Category Work

---
+++ src/app/pages/admin/category/category.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Injector } from '@angular/core';
+import { BaseService } from 'src/app/service/base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService extends BaseService<any> {
+
+  constructor(inject: Injector) {
+    super("products" , inject);
+  }
+
+  async getConfig(){
+    return await this.Get("getCategory");
+  }
+
+  async saveCategory(body: any){
+    return await this.Post("addCategory" , body);
+  }
+
+  async editCategory(body: any){
+    return await this.Post("editCategory" , body);
+  }
+
+  async deleteCategory(id: number){
+    return await this.Get("deleteCategory" , id);
+  }
+}
